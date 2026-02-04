0f52275a99cf88df6241ca9382be15b9f1cb366f
Jean Cochrane (Senior Backend Engineer, Sendwave) <jean@jeancochrane.com>
2026-02-03 20:38:01 -0600

    WIP setup digitalocean deployment

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -5,4 +5,16 @@ if [ "$DJANGO_MANAGEPY_MIGRATE" = 'on' ]; then
     python manage.py migrate --noinput
 fi
 
-exec "$@"
+if [ "$DJANGO_ENV" = 'prod' ]; then
+    nginx -c /app/app/nginx.conf &
+    exec gunicorn mbm.wsgi:application \
+        --bind 127.0.0.1:8000 \
+        --workers 2 \
+        --threads 4 \
+        --timeout 60 \
+        --access-logfile - \
+        --error-logfile - \
+        --log-level info
+else
+    exec python manage.py runserver 0.0.0.0:8000
+fi
