279a08045d47a7a0e5988b165dcc2a9195494fe0
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2026-05-12 00:08:02 -0500

    Use correct param name

--- app/mbm/middleware.py
+++ app/mbm/middleware.py
@@ -19,7 +19,7 @@ class RequestLoggingMiddleware():
         extra = {"duration": duration, "method": request.method,
                  "path": request.path, "status": response.status_code}
 
-        logged_query_params = ["source", "target", "bbox"]
+        logged_query_params = ["source", "target", "showBbox"]
         for key in logged_query_params:
             value = request.GET.get(key)
             if value:
