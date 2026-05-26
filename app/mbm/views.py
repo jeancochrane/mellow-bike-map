d30f9015b6b9f81117bae84b9cac3f1c7d745dd2
Jean Cochrane (Senior Backend Engineer, Sendwave) <jean@jeancochrane.com>
2026-05-26 18:22:43 -0500

    Add missing `request` arg to new functional `pong()` endpoint

--- app/mbm/views.py
+++ app/mbm/views.py
@@ -583,7 +583,7 @@ def server_error(request, template_name='mbm/500.html'):
     return render(request, template_name, status=500)
 
 
-def pong():
+def pong(request):
     from settings import DEPLOYMENT_ID
     return HttpResponse(DEPLOYMENT_ID)
 
